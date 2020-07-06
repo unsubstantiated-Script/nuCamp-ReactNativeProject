9f39d3b8d62938c337b69ae3c8c5c9be5de95fb1
Justin Fulton <jusfulton@gmail.com>
2020-07-06 08:47:32 -0700

    Icons, Favorites, and Comments

---
+++ components/ContactComponent.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import { FlatList, Text, ScrollView } from "react-native";
+import { Card, ListItem } from "react-native-elements";
+import { PARTNERS } from "../shared/partners";
+
+class Contact extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      partners: PARTNERS,
+    };
+  }
+
+  static navigationOptions = {
+    title: "Contact Us",
+  };
+
+  render() {
+    return (
+      <ScrollView>
+        <Card title="Contact Information" wrapperStyle={{ margin: 20 }}>
+          <Text>1 Nucamp Way</Text>
+          <Text>Seattle, WA 98001</Text>
+          <Text style={{ marginBottom: 10 }}>U.S.A.</Text>
+          <Text>Phone: 1-206-555-1234</Text>
+          <Text>Email: campsites@nucamp.co</Text>
+        </Card>
+      </ScrollView>
+    );
+  }
+}
+
+export default Contact;

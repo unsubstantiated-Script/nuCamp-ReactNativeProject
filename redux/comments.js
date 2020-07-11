77452eecaec8aeae27bcbb2306be36af540bb26a
Justin Fulton <jusfulton@gmail.com>
2020-07-11 12:52:37 -0700

    adding the comment module

--- redux/comments.js
+++ redux/comments.js
@@ -5,6 +5,10 @@ export const comments = (state = { errMess: null, comments: [] }, action) => {
     case ActionTypes.ADD_COMMENTS:
       return { ...state, errMess: null, comments: action.payload };
 
+    case ActionTypes.ADD_COMMENT:
+      const newComment = { ...action.payload, id: state.comments.length };
+      return { ...state, comments: state.comments.concat(newComment) };
+
     case ActionTypes.COMMENTS_FAILED:
       return { ...state, errMess: action.payload };
 

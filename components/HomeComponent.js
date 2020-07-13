443fd20d7210967730ba5309f202ebb0fc4b2784
Justin Fulton <jusfulton@gmail.com>
2020-07-13 15:57:22 -0700

    Animations Mickey!

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
-import { View, Text, ScrollView } from "react-native";
 import { Card } from "react-native-elements";
 import { connect } from "react-redux";
 import { baseUrl } from "../shared/baseUrl";
 import Loading from "./LoadingComponent";
+import { View, Text, Animated } from "react-native";
 
 const mapStateToProps = (state) => {
   return {
@@ -39,13 +39,33 @@ function RenderItem(props) {
 }
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      scaleValue: new Animated.Value(0),
+    };
+  }
+
+  animate() {
+    Animated.timing(this.state.scaleValue, {
+      toValue: 1,
+      duration: 1500,
+    }).start();
+  }
+
+  componentDidMount() {
+    this.animate();
+  }
+
   static navigationOptions = {
     title: "Home",
   };
 
   render() {
     return (
-      <ScrollView>
+      <Animated.ScrollView
+        style={{ transform: [{ scale: this.state.scaleValue }] }}
+      >
         <RenderItem
           item={
             this.props.campsites.campsites.filter(
@@ -73,7 +93,7 @@ class Home extends Component {
           isLoading={this.props.partners.isLoading}
           errMess={this.props.partners.errMess}
         />
-      </ScrollView>
+      </Animated.ScrollView>
     );
   }
 }
--- components/MainComponent.js
+++ components/MainComponent.js
@@ -27,6 +27,7 @@ import {
   fetchPromotions,
   fetchPartners,
 } from "../redux/ActionCreators";
+import Favorites from "./FavoritesComponent";
 
 const mapDispatchToProps = {
   fetchCampsites,
@@ -166,6 +167,31 @@ const ReservationNavigator = createStackNavigator(
   }
 );
 
+const FavoritesNavigator = createStackNavigator(
+  {
+    Favorites: { screen: Favorites },
+  },
+  {
+    navigationOptions: ({ navigation }) => ({
+      headerStyle: {
+        backgroundColor: "#5637DD",
+      },
+      headerTintColor: "#fff",
+      headerTitleStyle: {
+        color: "#fff",
+      },
+      headerLeft: (
+        <Icon
+          name="heart"
+          type="font-awesome"
+          iconStyle={styles.stackIcon}
+          onPress={() => navigation.toggleDrawer()}
+        />
+      ),
+    }),
+  }
+);
+
 const CustomDrawerContentComponent = (props) => (
   <ScrollView>
     <SafeAreaView
@@ -206,12 +232,23 @@ const MainNavigator = createDrawerNavigator(
         ),
       },
     },
+
     Reservation: {
       screen: ReservationNavigator,
       navigationOptions: {
         drawerLabel: "Reserve Campsite",
         drawerIcon: ({ tintColor }) => (
           <Icon name="tree" type="font-awesome" size={24} color={tintColor} />
+        ),
+      },
+    },
+
+    Favorites: {
+      screen: FavoritesNavigator,
+      navigationOptions: {
+        drawerLabel: "My Favorites",
+        drawerIcon: ({ tintColor }) => (
+          <Icon name="heart" type="font-awesome" size={24} color={tintColor} />
         ),
       },
     },
